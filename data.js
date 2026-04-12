97facad475720ed70dfc91eb4de305fbd6a3655d
TorresJim <torresguarino17@gmail.com>
2026-04-12 17:36:32 +0800

    Project tech update

--- data.js
+++ data.js
@@ -11,27 +11,27 @@ export default [
         link: 'https://torresjimboy.github.io/socialbook/',
         name: 'Socialbook',
         description: "An interactive Facebook-style social media clone designed to demonstrate skills in JavaScript, React, CSS, and Database integration.",
-        tech: 'JavaScript • TypeScript • React • Auth • Supabase'
+        tech: 'Bootstrap • TypeScript • React • Auth • Supabase'
     },
     {
         image: './img/orderingapp.png',
         link: 'https://torresjimboy.github.io/ordering-app/',
         name: 'Ordering App',
         description: "An interactive ordering app featuring medieval equipments, allowing users to explore and shop for historically inspired items.",
-        tech: 'JavaScript • TypeScript • React'
+        tech: 'HTML • TypeScript • React'
     },
     {
         image: './img/weatherapp.png',
         link: 'https://torresjimboy.github.io/weather-app/',
         name: 'Weather App',
         description: "Real-time weather insights in a modern, intuitive interface to help you plan your day effortlessly.",
-        tech: 'React • REST API • JavaScript'
+        tech: 'React • REST API • Typescript'
     },
     {
         image: './img/memorygame.png',
         link: 'https://torresjimboy.github.io/memory-game/',
         name: 'Memory Game',
         description: "A responsive Memory Game that challenges players to match card pairs, enhancing focus and memory skills with a clean, interactive interface.",
-        tech: 'JavaScript • TypeScript • React'
+        tech: 'HTML • CSS • TypeScript • React'
     }
 ]
